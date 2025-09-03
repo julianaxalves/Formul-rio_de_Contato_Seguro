c0bceb14f9265f24dc04064c12539eb0cf0050cf
julianaxalves <alvesjuliana944@gmail.com>
2025-09-03 17:21:45 -0300

    Add files via upload

---
+++ validar.js
@@ -0,0 +1,17 @@
+const form = document.getElementById("formx");
+
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    const dados = {
+        nome: nome,
+        email: email,
+        mensagem: mensagem
+    };
+
+    console.log(dados);
+});
